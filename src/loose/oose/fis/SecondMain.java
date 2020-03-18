cb5f5bbb342d641a6cb98fa84c681d42861f0a65
Flavius-Gabriel Brandeu <flavius.brandeu@gmail.com>
2020-03-18 22:57:30 +0200

    Corrected main method in SecondMain class

--- src/loose/oose/fis/SecondMain.java
+++ src/loose/oose/fis/SecondMain.java
@@ -1,7 +1,7 @@
 package loose.oose.fis;
 
 public class SecondMain {
-    public static void main(){
+    public static void main(String[] args){
         System.out.println("This is the new class created for testing git");
     }
 }
